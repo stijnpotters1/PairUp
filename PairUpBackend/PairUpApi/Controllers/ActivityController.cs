430e64efbbf76da8294bde84f76e95a812c0fbcd
Stijn Potters <spotters@lely.com>
2024-11-19 19:23:28 +0100

    added get activities controller api request

--- PairUpBackend/PairUpApi/Controllers/ActivityController.cs
+++ PairUpBackend/PairUpApi/Controllers/ActivityController.cs
@@ -1,6 +1,24 @@
 ﻿namespace PairUpApi.Controllers;
 
-public class ActivityController
+[Route("api/[controller]")]
+[ApiController]
+public class ActivityController : ControllerBase
 {
-    
+    private readonly IActivityRepository _repository;
+    private readonly IActivityService _service;
+
+    public ActivityController(IActivityRepository repository, IActivityService service)
+    {
+        _repository = repository;
+        _service = service;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<IEnumerable<ActivityResponse>>> GetActivities(
+        [FromBody] ActivityRequest requirements)
+    {
+        IEnumerable<Activity> activities = await _repository.GetActivitiesAsync(requirements);
+        IEnumerable<ActivityResponse> activityResponses = activities.Select(ac => _service.ConvertToResponse(ac));
+        return Ok(activityResponses);
+    }
 }
