8bafa82de47610e1a71afd9e978ade64354a3407
stijnpotters <stijn.potters1@gmail.com>
2024-11-19 19:22:28 +0100

    added categories to activity entity

--- PairUpBackend/PairUpApi/Migrations/20241119182044_added categories.Designer.cs
+++ PairUpBackend/PairUpApi/Migrations/20241119182044_added categories.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using PairUpApi.Data;
@@ -11,9 +12,11 @@ using PairUpApi.Data;
 namespace PairUpApi.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20241119182044_added categories")]
+    partial class addedcategories
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
