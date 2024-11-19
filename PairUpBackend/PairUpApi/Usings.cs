ef3675dc044f335c99f36167eab0af586f22030e
Stijn Potters <spotters@lely.com>
2024-11-19 19:22:44 +0100

    added usings

--- PairUpBackend/PairUpApi/Usings.cs
+++ PairUpBackend/PairUpApi/Usings.cs
@@ -3,9 +3,14 @@ global using PairUpApi.Data;
 global using PairUpApi.Entity;
 global using PairUpApi.Configuration;
 global using PairUpApi.Exceptions;
+global using PairUpApi.DTO.Requests;
+global using PairUpApi.DTO.Responses;
+global using PairUpApi.Repositories;
+global using PairUpApi.Service;
 
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.AspNetCore.Mvc;
 
 global using System.ComponentModel.DataAnnotations;
 global using System.ComponentModel.DataAnnotations.Schema;
