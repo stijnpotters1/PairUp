8c6a8d4a573160f47b6b53f7f6db1c40ba469661
stijnpotters <stijn.potters1@gmail.com>
2024-11-19 13:06:00 +0100

    added usings

--- PairUpBackend/PairUpApi/Usings.cs
+++ PairUpBackend/PairUpApi/Usings.cs
@@ -1,2 +1,14 @@
-﻿global using Microsoft.EntityFrameworkCore;
+﻿global using PairUpApi.Configuration.Services;
+global using PairUpApi.Data;
+global using PairUpApi.Entity;
+global using PairUpApi.Configuration;
+global using PairUpApi.Exceptions;
+
+global using Microsoft.EntityFrameworkCore;
 global using Microsoft.Extensions.DependencyInjection;
+
+global using System.ComponentModel.DataAnnotations;
+global using System.ComponentModel.DataAnnotations.Schema;
+
+global using AutoMapper;
+global using DotNetEnv;
