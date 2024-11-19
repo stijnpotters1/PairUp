cac02c17250eac63bb7ece19f5c647c58ded9df3
Stijn Potters <spotters@lely.com>
2024-11-19 13:06:21 +0100

    added entities and dto's

---
+++ PairUpBackend/PairUpApi/Entity/Activity.cs
@@ -0,0 +1,29 @@
+﻿namespace PairUpApi.Entity;
+
+[Table("activity")]
+public class Activity
+{
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Key]
+    public Guid Id { get; set; }
+
+    [StringLength(255)] 
+    public string Name { get; set; } = null!;
+
+    [StringLength(255)] 
+    public string Description { get; set; } = null!;
+
+    [StringLength(255)] 
+    public string Url { get; set; } = null!;
+
+    public double Price { get; set; }
+
+    [StringLength(255)]
+    public string Age { get; set; } = null!;
+
+    public string Duration { get; set; } = null!;
+    
+    public Guid PlaceId { get; set; }
+
+    public Place Place { get; set; } = null!;
+}
