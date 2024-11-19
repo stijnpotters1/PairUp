45488e5f661297da551afe359e6a549fc18c4ef1
Stijn Potters <spotters@lely.com>
2024-11-19 13:05:20 +0100

    added database configuration

--- PairUpBackend/PairUpApi/Data/DataContext.cs
+++ PairUpBackend/PairUpApi/Data/DataContext.cs
@@ -5,4 +5,7 @@ public class DataContext : DbContext
     public DataContext(DbContextOptions<DataContext> options) : base(options)
     {
     }
+    
+    public DbSet<Activity> Activities { get; set; }
+    public DbSet<Place> Places { get; set; }
 }
