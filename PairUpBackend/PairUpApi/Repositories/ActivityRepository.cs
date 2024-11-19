80083cd45b5ea318c8df153ccdd9b5b7ca349574
stijnpotters <stijn.potters1@gmail.com>
2024-11-19 19:24:07 +0100

    added initial activity repository methods

--- PairUpBackend/PairUpApi/Repositories/ActivityRepository.cs
+++ PairUpBackend/PairUpApi/Repositories/ActivityRepository.cs
@@ -2,5 +2,23 @@
 
 public class ActivityRepository : IActivityRepository
 {
+    private readonly DataContext _context;
+
+    public ActivityRepository(DataContext context)
+    {
+        _context = context;
+    }
     
+    public async Task<IEnumerable<Activity>> GetActivitiesAsync(ActivityRequest requirements)
+    {
+        return await _context.Activities
+            .Include(a => a.Category)
+            .Include(a => a.Place)
+            .ToListAsync();
+    }
+
+    private Task CalculateCoordinatesBasedOnUserLocation()
+    {
+        throw new NotImplementedException();
+    }
 }
