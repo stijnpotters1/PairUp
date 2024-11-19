c65c29345fc7e9e18cb0f5299bf9d7a9576e99d0
stijnpotters <stijn.potters1@gmail.com>
2024-11-19 19:23:00 +0100

    added service for activities

--- PairUpBackend/PairUpApi/Service/ActivityService.cs
+++ PairUpBackend/PairUpApi/Service/ActivityService.cs
@@ -1,6 +1,17 @@
-﻿namespace PairUpApi.Service;
+﻿
+namespace PairUpApi.Service;
 
-public class ActivityService : IService
+public class ActivityService : IActivityService
 {
+    private readonly IMapper _mapper;
+
+    public ActivityService(IMapper mapper)
+    {
+        _mapper = mapper;
+    }
     
+    public ActivityResponse ConvertToResponse(Activity activity)
+    {
+        return _mapper.Map<ActivityResponse>(activity);
+    }
 }
