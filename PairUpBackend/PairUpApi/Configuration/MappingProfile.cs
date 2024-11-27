703c73671d81664f26ce9b2de4ed7c92e83a4dd0
stijnpotters <stijn.potters1@gmail.com>
2024-11-27 13:04:26 +0100

    changed ActivityResponse category to many, including the right mapping

--- PairUpBackend/PairUpApi/Configuration/MappingProfile.cs
+++ PairUpBackend/PairUpApi/Configuration/MappingProfile.cs
@@ -4,6 +4,9 @@ public class MappingProfile : Profile
 {
     public MappingProfile()
     {
-        CreateMap<Activity, ActivityResponse>();
+        CreateMap<Activity, ActivityResponse>()
+            .ForMember(dest => dest.SubLevelCategories, opt => opt.MapFrom(src => src.ActivitySubLevelCategories.Select(asc => asc.SubLevelCategory)));
+
+        CreateMap<SubLevelCategory, SubLevelCategoryResponse>();
     }
 }
