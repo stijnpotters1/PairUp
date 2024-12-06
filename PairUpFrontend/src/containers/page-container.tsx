3ada4a5e0238d1bea83ee124140c7da9707566f9
Stijn Potters <spotters@lely.com>
2024-12-06 13:57:49 +0100

    added navigation and initial container

---
+++ PairUpFrontend/src/containers/page-container.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const PageContainer: React.FunctionComponent = () => {
+    return (
+        <div className="container-fluid px-0 min-vh-100">
+            <div className="d-flex flex-column p-3 navigation-padding min-vh-100">
+                {/*components*/}
+            </div>
+        </div>
+    );
+}
+
+export default PageContainer;
