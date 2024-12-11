3736ad2a975a0f3d709d2c2ddeeaa1adf6238888
stijnpotters <stijn.potters1@gmail.com>
2024-12-11 15:26:38 +0100

    added final logics for this component and updated bugged spinner

--- PairUpFrontend/src/containers/card-container.tsx
+++ PairUpFrontend/src/containers/card-container.tsx
@@ -6,7 +6,7 @@ interface CardContainerProps {
 
 const CardContainer: React.FunctionComponent<CardContainerProps> = ({children}) => {
     return (
-        <div className="d-flex flex-column">
+        <div className="d-flex flex-column custom-card-container">
             {children}
         </div>
     );
