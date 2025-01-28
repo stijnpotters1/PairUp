edbb4ed8fdba9d2bf6f7d884cac153b1d608b3d5
stijnpotters <stijn.potters1@gmail.com>
2025-01-28 18:58:17 +0100

    added private route for auth validation

---
+++ PairUpFrontend/src/components/private-route/private-route.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import { useUser } from '../../hooks/user-auth';
+
+interface PrivateRouteProps {
+    element: React.ReactElement;
+    roles?: string[];
+}
+
+const PrivateRoute: React.FC<PrivateRouteProps> = ({ element, roles }) => {
+    const { getUser, loading } = useUser();
+    const location = useLocation();
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    const user = getUser();
+
+    if (!user) {
+        return <Navigate to="/radar/recipe" state={{ from: location }} />;
+    }
+
+    if (roles && !roles.includes(user.role.name)) {
+        return <Navigate to="/radar/recipe" state={{ from: location }} />;
+    }
+
+    return element;
+};
+
+export default PrivateRoute;
