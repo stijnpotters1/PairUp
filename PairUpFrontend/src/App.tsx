a1aed4e4254380890e902f0818f0c5581e1b1024
stijnpotters <stijn.potters1@gmail.com>
2025-01-28 19:19:51 +0100

    added and updated all frontend routing

--- PairUpFrontend/src/App.tsx
+++ PairUpFrontend/src/App.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Navigation from './components/navigation/navigation';
 import PageContainer from './containers/page-container';
 import {UserProvider} from "./hooks/user-auth";
 import Users from "./components/users/users";
 import PrivateRoute from "./components/private-route/private-route";
+import Profile from "./components/profile/profile";
+import {Activity} from "./models/activity";
+import Register from "./components/register/register";
+import Login from "./components/login/login";
 
 const App: React.FC = () => {
+    const [activities, setActivities] = useState<Activity[]>([]);
+
     return (
         <UserProvider>
             <Router>
@@ -14,11 +20,12 @@ const App: React.FC = () => {
                     <Navigation />
                     <Routes>
                         <Route path="/*" element={<Navigate to="/trips" />} />
-                        <Route path="/trips" element={<PageContainer />} />
+                        <Route path="/trips" element={<PageContainer setActivities={setActivities} />} />
+                        <Route path="/liked" element={<PrivateRoute element={<PageContainer activities={activities} />} roles={['User', 'Admin']} />} />
                         <Route path="/users" element={<PrivateRoute element={<Users />} roles={['Admin']} />} />
-                        <Route path="/register" element={<Navigate to="/register" />} />
-                        <Route path="/login" element={<Navigate to="/login" />} />
-                        <Route path="/profile" element={<Navigate to="/profile" />} />
+                        <Route path="/profile" element={<PrivateRoute element={<Profile />} roles={['User', 'Admin']} />} />
+                        <Route path="/register" element={<Register />} />
+                        <Route path="/login" element={<Login />} />
                     </Routes>
                 </div>
             </Router>
