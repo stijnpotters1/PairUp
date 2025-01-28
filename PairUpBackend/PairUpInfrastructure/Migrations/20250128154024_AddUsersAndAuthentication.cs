af0fa908b871f8a09c5267db3e6e9e6f9bea0a16
stijnpotters <stijn.potters1@gmail.com>
2025-01-28 17:09:34 +0100

    feat: added missing auth migration

---
+++ PairUpBackend/PairUpInfrastructure/Migrations/20250128154024_AddUsersAndAuthentication.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PairUpInfrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUsersAndAuthentication : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId",
+                table: "activity",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "role",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_role", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    FirstName = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true),
+                    LastName = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true),
+                    Email = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Password = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    RoleId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_user_role_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "role",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_activity_UserId",
+                table: "activity",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_RoleId",
+                table: "user",
+                column: "RoleId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_activity_user_UserId",
+                table: "activity",
+                column: "UserId",
+                principalTable: "user",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_activity_user_UserId",
+                table: "activity");
+
+            migrationBuilder.DropTable(
+                name: "user");
+
+            migrationBuilder.DropTable(
+                name: "role");
+
+            migrationBuilder.DropIndex(
+                name: "IX_activity_UserId",
+                table: "activity");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "activity");
+        }
+    }
+}
