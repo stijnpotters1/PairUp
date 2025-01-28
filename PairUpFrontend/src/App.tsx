8aa8a4e89df69f0a1ea1339004ad94ff06a62b59
stijnpotters <stijn.potters1@gmail.com>
2025-01-28 18:57:39 +0100

    added extra paths and added hook

--- PairUpFrontend/src/App.tsx
+++ PairUpFrontend/src/App.tsx
@@ -2,18 +2,27 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Navigation from './components/navigation/navigation';
 import PageContainer from './containers/page-container';
+import {UserProvider} from "./hooks/user-auth";
+import Users from "./components/users/users";
+import PrivateRoute from "./components/private-route/private-route";
 
 const App: React.FC = () => {
     return (
-        <Router>
-            <div className="d-flex flex-column">
-                <Navigation />
-                <Routes>
-                    <Route path="/*" element={<Navigate to="/trips" />} />
-                    <Route path="/trips" element={<PageContainer />} />
-                </Routes>
-            </div>
-        </Router>
+        <UserProvider>
+            <Router>
+                <div className="d-flex flex-column">
+                    <Navigation />
+                    <Routes>
+                        <Route path="/*" element={<Navigate to="/trips" />} />
+                        <Route path="/trips" element={<PageContainer />} />
+                        <Route path="/users" element={<PrivateRoute element={<Users />} roles={['Admin']} />} />
+                        <Route path="/register" element={<Navigate to="/register" />} />
+                        <Route path="/login" element={<Navigate to="/login" />} />
+                        <Route path="/profile" element={<Navigate to="/profile" />} />
+                    </Routes>
+                </div>
+            </Router>
+        </UserProvider>
     );
 };
 
