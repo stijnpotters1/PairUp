0000b6b7da2c5d8ada7db0af04c2c0e3b5b45068
stijnpotters <stijn.potters1@gmail.com>
2025-01-29 19:16:18 +0100

    added top level controller and interface for liking an activity

--- PairUpBackend/PairUpApi/Controllers/ActivityController.cs
+++ PairUpBackend/PairUpApi/Controllers/ActivityController.cs
@@ -5,11 +5,13 @@
 public class ActivityController : ControllerBase
 {
     private readonly IActivityRepository _repository;
-    private readonly IServices<Activity, ActivityResponse> _service;
+    private readonly IServices<Activity, ActivityResponse> _services;
+    private readonly IService<Activity, ActivityResponse> _service;
 
-    public ActivityController(IActivityRepository repository, IServices<Activity, ActivityResponse> service)
+    public ActivityController(IActivityRepository repository, IServices<Activity, ActivityResponse> services, IService<Activity, ActivityResponse> service)
     {
         _repository = repository;
+        _services = services;
         _service = service;
     }
 
@@ -21,12 +23,29 @@ public class ActivityController : ControllerBase
 
         var activityResponse = new PagedActivityResponse<ActivityResponse>
         {
-            Items = _service.ConvertToResponse(activities.Items),
+            Items = _services.ConvertToResponse(activities.Items),
             TotalCount = activities.TotalCount,
             PageNumber = activities.PageNumber,
             PageSize = activities.PageSize
         };
 
         return Ok(activityResponse);
+    }
+    
+    [HttpPost("like/{userId}/activity/{activityId}")]
+    [Authorize(Policy = "UserOrAdmin")]
+    public async Task<ActionResult<ActivityResponse>> LikeActivity(Guid userId, Guid activityId)
+    {
+        Activity likedActivity = await _repository.AddLikeAsync(userId, activityId);
+        ActivityResponse likedActivityResponse = _service.ConvertToResponse(likedActivity);
+        return Ok(likedActivityResponse);
+    }
+
+    [HttpDelete("unlike/{userId}/activity/{activityId}")]
+    [Authorize(Policy = "UserOrAdmin")]
+    public async Task<ActionResult<bool>> UnlikeActivity(Guid userId, Guid activityId)
+    {
+        var response = await _repository.RemoveLikeAsync(userId, activityId);
+        return Ok(response);
     }
 }
--- PairUpBackend/PairUpCore/Interfaces/IActivityRepository.cs
+++ PairUpBackend/PairUpCore/Interfaces/IActivityRepository.cs
@@ -3,4 +3,6 @@
 public interface IActivityRepository
 {
     Task<PagedActivityResponse<Activity>> GetActivitiesAsync(ActivityRequest requirements);
+    Task<Activity> AddLikeAsync(Guid userId, Guid activityId);
+    Task<bool> RemoveLikeAsync(Guid userId, Guid activityId); 
 }
